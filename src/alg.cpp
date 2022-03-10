e29618389b3fbddfbaade3547c929bf8a32a0a8f
AleksandraBogolepova <99176112+AleksandraBogolepova@users.noreply.github.com>
2022-03-10 20:59:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,22 +3,18 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
-  int i, j, a, mid;
-  a = 0;
-  i = 0;
-  j = size - 1;
-  mid = i + (j - 1) / 2;
-  if (true) {
-    for (i = 0; i < mid; i++) {
-      if (arr[i] == value)
-        a++;
-    }
-    for (j = size; j >= mid; j--) {
-      if (arr[j] == value)
-        a++;
-    }
-    return a;
-  } else {
+  int i = 0, j = size -1, a = 0;
+ int mid = i + (j - 1) / 2;
+  if (size == 0) {
     return 0;
   }
+  if (arr[mid] == value) {
+    a++;
+  } else if (arr[mid] > value) {
+    j = mid;
+  } else {
+    i = mid;
+  }
+  return a + cbinsearch(arr, i, value) + cbinsearch(&(arr[mid + 1]), j - mid, value);
 }
+
